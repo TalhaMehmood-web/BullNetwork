823ee0dad21a2bea9cfba602c3924024025cd67e
TalhaMehmood <talhamehmood991@gmail.com>
2024-02-07 20:56:53 +0500

    commenting

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,3 +1,4 @@
+// import files and decencies
 import mongoose from "mongoose";
 import User from "../models/userModel.js";
 import asyncHandler from "express-async-handler"
@@ -6,7 +7,7 @@ import validator from "validator";
 import bcrypt from "bcryptjs";
 import ApiResponse from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken"
-
+// JWT Token Generator Function
 let createToken = (_id, username, email, phoneNumber) => {
     const payload = {
         _id,
@@ -16,6 +17,8 @@ let createToken = (_id, username, email, phoneNumber) => {
     };
     return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '30d' });
 };
+//------------------------------------
+// Sign up user API controller
 export const signUpUser = asyncHandler(async (req, res) => {
     try {
         const { username, email, phoneNumber, password, confirmPassword } = req.body;
@@ -28,14 +31,14 @@ export const signUpUser = asyncHandler(async (req, res) => {
         if (!validator.isEmail(email)) {
             throw new ApiError(400, "Email is not in valid format!!")
         }
-        const existedUser = await User.findOne({
+        const existedUser = await User.findOne({      // user validation 
             $or: [{ phoneNumber }, { email }]
         });
         if (existedUser) {
             throw new ApiError(409, "User with email or phone number already exists")
         }
         const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, salt);
+        const hashedPassword = await bcrypt.hash(password, salt); // password hashing 
         const user = await User.create({
             username,
             email,
@@ -53,6 +56,8 @@ export const signUpUser = asyncHandler(async (req, res) => {
         throw new ApiError(500, error?.message)
     }
 })
+// -----------------------------------
+// login user Api Controller 
 export const loginUser = asyncHandler(async (req, res) => {
     try {
         const { email, password } = req.body;
@@ -63,11 +68,11 @@ export const loginUser = asyncHandler(async (req, res) => {
         if (!user) {
             throw new ApiError(404, "User Not Found with this email!")
         }
-        const matchPassword = await bcrypt.compare(password, user.password);
+        const matchPassword = await bcrypt.compare(password, user.password); // matching hashed password
         if (!matchPassword) {
             throw new ApiError(400, "Incorrect Password")
         }
-        const token = createToken(user._id, user.username, user.email, user.phoneNumber)
+        const token = createToken(user._id, user.username, user.email, user.phoneNumber) // assigning token 
         return res.status(201).json(new ApiResponse(200, user, "User LoggedIn Successfully", token))
     } catch (error) {
         throw new ApiError(500, error?.message)
--- src/db/index.js
+++ src/db/index.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constant.js";
-
+// DB Connection function 
 const connectDB = async () => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
